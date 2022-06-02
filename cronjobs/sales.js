c308ecd403bd07acb5ff2187f05e1ffc8443e903
yukairex <wyktju@gmail.com>
2022-06-02 14:46:54 +0800

    update last stamp

--- cronjobs/sales.js
+++ cronjobs/sales.js
@@ -14,7 +14,7 @@ module.exports = {
   async execute(client) {
     if (lastTimestamp == null) {
       lastTimestamp = Math.floor(Date.now() / 1000) - 120;
-      lastTimestamp = 1653746603; // initial deployment
+      //lastTimestamp = 1653746603; // initial deployment
     } else {
       lastTimestamp -= 30;
     }
